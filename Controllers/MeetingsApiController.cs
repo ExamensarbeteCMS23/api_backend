40b4d18918f8179b032904e01163aac35e0ad48b
TinaLutti <143405689+TinaLutti@users.noreply.github.com>
2025-04-29 09:30:32 +0200

    Started on MeetingsApiController

---
+++ Controllers/MeetingsApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MeetingsApiController : ControllerBase
+    {
+        [HttpGet]  
+        public IActionResult Index()
+        {
+            return Ok("API fungerar!");
+        }
+    }
+}
